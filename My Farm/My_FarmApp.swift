e6a015c6e9403a12c2892d31110b2f7321b74f69
Mazen Eldeeb <157023796+mazeneldeeb9@users.noreply.github.com>
2025-02-16 17:48:53 +0200

    Initial Commit

---
+++ My Farm/My_FarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_FarmApp.swift
+//  My Farm
+//
+//  Created by mazen eldeeb on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct My_FarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
